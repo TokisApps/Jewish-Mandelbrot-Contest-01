74cbd58e719bcbe73cf92927a809417b1e5a50ca
Thorsten Kiefer <97814848+TokisApps@users.noreply.github.com>
2022-11-23 18:50:08 +0100

    Add files via upload

--- gensent.sh
+++ gensent.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-generateSentence.exe | trans de:en -b | trans en:de -b
+str="`generateSentence.exe | grep -v seed`"
+str2="`echo $str | trans de:en -b | trans en:de -b`"
+
+if [ -z "$str2" ]; then
+	echo $str
+else
+	echo $str2
+fi
 
 
-#//Dynamic Signature : Ein Diebesbrot meiner zinslosen Ampel haben mehrere Chemie-Erwachsene meiner öffentlichen Luft geschuldet.
+#//Dynamic Signature : Du negativer August mehrerer poetischer Bücher schiebst das solide Gepäck unserer reinen Malerei.
+#//Dynamic Signature : Die zerstreuten Meere meiner egoistischen Angst zeigen den jährlichen Strang meines gegenwärtigen Stuhls.
