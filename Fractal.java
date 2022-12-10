c4c5934c9d6d6cfbc11cf11d44e34c4e8534c05a
Thorsten Kiefer <97814848+TokisApps@users.noreply.github.com>
2022-12-10 07:31:30 +0100

    Add files via upload

--- Fractal.java
+++ Fractal.java
@@ -2,7 +2,10 @@ import org.apache.commons.numbers.complex.*;
 import javax.swing.*;
 import java.awt.image.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
+import java.io.*;
+import javax.imageio.*;
 
 
 public class Fractal {
@@ -54,8 +57,8 @@ public class Fractal {
 	}
 
 	class MyComponent extends JComponent {
-			final int w = 200;
-			final int h = 200;
+			final int w = 300;
+			final int h = 300;
 			double[][] sums = new double[w][h];
 			double[] rs = new double[] {
 				Math.random(),
@@ -67,10 +70,14 @@ public class Fractal {
 			};
 			double summin;
 			double summax;
-			double mxmin = -10;
-			double mxmax = 10;
-			double mymin = -10;
-			double mymax = 10;
+			double mxmin = -13.1 * (Math.random() - 0.5);
+			double mxmax = 13.1 * (Math.random() - 0.5);
+			double mymin = -13.1 * (Math.random() - 0.5);
+			double mymax = 13.1 * (Math.random() - 0.5);
+			double mxminv = 0.1 * (Math.random() - 0.5);
+			double mxmaxv = 0.1 * (Math.random() - 0.5);
+			double myminv = 0.1 * (Math.random() - 0.5);
+			double mymaxv = 0.1 * (Math.random() - 0.5);
 
 			BufferedImage img = new BufferedImage(w,h,BufferedImage.TYPE_INT_RGB);
 			MyThread[] ts = new MyThread[] {
@@ -122,27 +129,31 @@ public class Fractal {
 
 
 
-        cv = Complex.ofCartesian((Math.random() - 0.5),(Math.random() - 0.5)).multiply(0.1).add(cv);
+        cv = Complex.ofCartesian((Math.random() - 0.5),(Math.random() - 0.5)).multiply(0.01).add(cv);
         c2v = Complex.ofCartesian((Math.random() - 0.5),(Math.random() - 0.5)).multiply(0.01).add(c2v);
         c3v = Complex.ofCartesian((Math.random() - 0.5),(Math.random() - 0.5)).multiply(0.01).add(c3v);
-        c = c.add(cv.multiply(0.001));
-        c2 = c2.add(c2v.multiply(0.001));
-        c3 = c3.add(c3v.multiply(0.001));
+        c = c.add(cv.multiply(0.01));
+        c2 = c2.add(c2v.multiply(0.01));
+        c3 = c3.add(c3v.multiply(0.01));
+
+	mxminv += 0.1 * (Math.random() - 0.5);
+	mxmaxv += 0.1 * (Math.random() - 0.5);
+	myminv += 0.1 * (Math.random() - 0.5);
+	mymaxv += 0.1 * (Math.random() - 0.5);
+
+	mxmin += 0.1 * mxminv;
+	mxmax += 0.1 * mxmaxv;
+	mymin += 0.1 * myminv;
+	mymax += 0.1 * mymaxv;
 
 		 rs = new double[] {
+				Math.random() + 0.5,
 				Math.random(),
+				Math.random() + 0.5,
 				Math.random(),
-				Math.random(),
-				Math.random(),
-				Math.random(),
+				Math.random() + 0.5,
 				Math.random()
 			};
-
-			repaint();
-
-			}
-
-			public void paintComponent(Graphics g) {
 				for(int x = 0;x < w;++x) {
 					for(int y = 0;y < h;++y) {
 						double t = (sums[x][y] - summin) / (summax - summin);
@@ -155,7 +166,14 @@ public class Fractal {
 						);
 					}
 				}
+
+			repaint();
+
+			}
+
+			public void paintComponent(Graphics g) {
 				g.drawImage(img,0,0,getWidth(),getHeight(),null);
+				
 
 			}
 		}
@@ -181,17 +199,63 @@ public class Fractal {
 		f.getContentPane().setLayout(new BorderLayout());
 		f.getContentPane().add(comp,BorderLayout.CENTER);
 
+		JButton b = new JButton("Save");
+		f.getContentPane().add(b,BorderLayout.SOUTH);
+
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String fn = "";
+				ArrayList<Character> xs = new ArrayList<>();
+				Random rnd = new Random();
+
+				for(int i = 0;i < 26;++i) {
+					xs.add((char)('a' + i));
+					xs.add((char)('Z' + i));
+				}
+
+				for(int i = 0;i < 10;++i) {
+					xs.add((char)('0' + i));
+				}
+
+				for(int i = 0;i < 10;++i) {
+					fn += xs.get(rnd.nextInt(xs.size()));
+				}
+
+				fn += ".jpg";
+
+				File f = new File(fn);
+
+				try {
+					ImageIO.write(comp.img, "JPG", f);
+				} catch(Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+
 		final java.util.Timer t = new java.util.Timer();
 		t.scheduleAtFixedRate(new TimerTask() {
 			public void run() {
 				comp.compute();
+
+				File f = new File("background.jpg");
+
+				try {
+					ImageIO.write(comp.img, "JPG", f);
+				} catch(Exception ex) {
+					ex.printStackTrace();
+				}
 			}
-		},0,100);
+		},0,5000);
 
 		f.setSize(800,600);
-		f.setVisible(true);
+		//f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		try {
+			Thread.sleep(100);
+		} catch(Exception ex) {
+		}
 	}
 }
 
